9e84f6967b1840ca78eae867bd63ce61e468fdbf
aparna satheesh <aparnamsatheesh@gmail.com>
2023-01-04 13:55:46 +0530

    Undo Moving commit

--- src/main/scala/com/aparna/scala/AddCharTest.scala
+++
@@ -1,7 +0,0 @@
-package com.aparna.scala
-import org.scalatest.funsuite.AnyFunSuite
-class AddCharTest extends AnyFunSuite {
-  test("AddCharTest.t1") {
-    assert(AddChar.addCharacter("Buy", "b" )=== "bBuyb")
-  }
-}

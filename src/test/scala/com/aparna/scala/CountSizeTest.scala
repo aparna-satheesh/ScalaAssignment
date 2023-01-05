f2e46b2acd84ae566101dc94509965139865e752
aparna satheesh <aparnamsatheesh@gmail.com>
2023-01-05 17:48:44 +0530

    CountSizeTest added

--- src/test/scala/com/aparna/scala/CountSizeTest.scala
+++ src/test/scala/com/aparna/scala/CountSizeTest.scala
@@ -2,7 +2,7 @@ package com.aparna.scala
 import org.scalatest.funsuite.AnyFunSuite
 class CountSizeTest extends AnyFunSuite {
   test("CountSizeTest.t1") {
-    assert(CountSize.countSize(Array(1,1,1,1,1,1,1,1,1,1)) === 10 )
+    assert(CountSize.countSize(Array(1,1,1,1,1,1,1,1,1)) === 9 )
     assert(CountSize.countSize(Array()) === 0 )
   }
 }
